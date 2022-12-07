f58ee10c041105eb9a0c51a02f871935f5cabb4e
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-07 03:01:02 -0300

    Tentando usar require para o cors

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./src/routes";
 import * as dotenv from 'dotenv'
-
+var cors = require('cors')
 dotenv.config()
 const app = express();
 
