fb9045d44dcd11efa9b62a0b26ed3de9fc9cdb0a
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-07 02:37:55 -0300

    Adding cors

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./src/routes";
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 const app = express();
+
+// let corsOptions = {
+//     origin: ['']
+// };
+
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
@@ -13,6 +19,7 @@ const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.7yvwjei.mongodb.net/?ret
 mongoose.connect(uri)
 mongoose.Promise = global.Promise;
 
+// app.use(cors(corsOptions))
 app.use(express.json());
 app.set("port", process.env.PORT || 3000);
 app.use(routes);
