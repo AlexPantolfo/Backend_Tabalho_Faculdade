b22622e96ed56e42902432936aa5a6695baf6d54
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-09 00:35:19 -0300

    Tentativa cors

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv'
 const cors = require('cors');
 dotenv.config()
 const app = express();
+app.use(cors());
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
@@ -14,8 +15,21 @@ const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.7yvwjei.mongodb.net/?ret
 mongoose.connect(uri)
 mongoose.Promise = global.Promise;
 
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
 app.use(express.json());
-app.use(cors())
+app.use(allowCrossDomain);
+app.options('*', cors());
 app.set("port", process.env.PORT || 3000);
 app.use(routes);
 
