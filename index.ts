2e112462ec93da0cf2630a5b12be66886e407ae6
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-09 00:56:50 -0300

    Resolução CORS apontando para o ambiente dev

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import * as dotenv from 'dotenv'
 const cors = require('cors');
 dotenv.config()
 const app = express();
-app.use(cors());
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
@@ -15,21 +14,8 @@ const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.7yvwjei.mongodb.net/?ret
 mongoose.connect(uri)
 mongoose.Promise = global.Promise;
 
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
-};
 app.use(express.json());
-app.use(allowCrossDomain);
-app.options('*', cors());
+app.use(cors({ origin: "https://trabalho-faculdade-gray.vercel.app", credentials: true }))
 app.set("port", process.env.PORT || 3000);
 app.use(routes);
 
