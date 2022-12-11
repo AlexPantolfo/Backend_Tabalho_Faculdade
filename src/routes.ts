60ca8025e813591b971ee1d7db1cfbe399e3ae34
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-11 12:09:53 -0300

    Criando rota auth

--- src/routes.ts
+++ src/routes.ts
@@ -26,8 +26,20 @@ routes.get("/", (req: Request, res: Response) => {
     res.json({ message: "Hello world!" });
 });
 
-routes.get("/teste", checkToken, (req: Request, res: Response) => {
-    res.json({ message: "essa rota funciona" });
+routes.get("/auth", (req: Request, res: Response) => {
+    const token = req.headers['authorization'];
+
+    if (!token) return res.status(401).json({ msg: "Acesso negado!" }).end();
+
+    try {
+        const secret = process.env.SECRET;
+
+        jwt.verify(token, secret);
+        res.status(200).json({ msg: "O Token é valido!" }).end();
+
+    } catch (err) {
+        res.status(400).json({ msg: "O Token é inválido!" }).end();
+    }
 });
 
 routes.post('/addUser', userController.addUser)
