7f30ef09ecbe7bc58d58969cc618db398586d7eb
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-11 21:17:46 -0300

    Habilitando CORS para mais de uma url

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv'
 const cors = require('cors');
 dotenv.config()
 const app = express();
-
+var allowedDomains = ['http://127.0.0.1:5500', 'https://trabalho-faculdade-gray.vercel.app'];
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
@@ -15,7 +15,19 @@ mongoose.connect(uri)
 mongoose.Promise = global.Promise;
 
 app.use(express.json());
-app.use(cors({ origin: "http://127.0.0.1:5500, https://trabalho-faculdade-gray.vercel.app", credentials: true }))
+app.use(cors({
+    origin: function (origin, callback) {
+        // bypass the requests with no origin (like curl requests, mobile apps, etc )
+        if (!origin) return callback(null, true);
+
+        if (allowedDomains.indexOf(origin) === -1) {
+            var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }, credentials: true
+}))
+
 app.set("port", process.env.PORT || 3000);
 app.use(routes);
 
