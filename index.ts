1256b546a4a4c841f67f3d4e427b618c98c10fa3
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2022-12-14 21:24:13 -0300

    Mudando permissão mongoDB

--- index.ts
+++ index.ts
@@ -9,24 +9,12 @@ var allowedDomains = ['http://127.0.0.1:5500', 'https://trabalho-faculdade-gray.
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
-const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.7yvwjei.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.epwyh8h.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(uri)
 mongoose.Promise = global.Promise;
 
 app.use(express.json());
-app.use(cors({
-    origin: function (origin, callback) {
-        // bypass the requests with no origin (like curl requests, mobile apps, etc )
-        if (!origin) return callback(null, true);
-
-        if (allowedDomains.indexOf(origin) === -1) {
-            var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }, credentials: true
-}))
+app.use(cors())
 
 app.set("port", process.env.PORT || 3000);
 app.use(routes);
